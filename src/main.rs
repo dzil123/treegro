28f24513dc5dac052de76f1e8582c9e571735ec8
dzil123 <5725958+dzil123@users.noreply.github.com>
2022-05-01 20:23:07 -0700

    resize pixels

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![forbid(unsafe_code)]
 
+use pixels::Pixels;
 use treegro::{cell::Cell, *};
 
 use rand::prelude::*;
@@ -11,11 +12,14 @@ struct World {
     cells: Vec<Cell>,
     time_delta: f32,
     growth_rate: f32,
+
+    pixels_size: (u32, u32),
 }
 
 impl World {
     fn new() -> Self {
         let mut this = Self {
+            pixels_size: (WIDTH, HEIGHT),
             ..Default::default()
         };
         this.randomize();
@@ -39,7 +43,9 @@ impl World {
 }
 
 impl App for World {
-    fn update(&mut self, frame: &mut [u8], ctx: &egui::Context) {
+    fn update(&mut self, pixels: &mut Pixels, ctx: &egui::Context) {
+        let mut changed_size = false;
+
         egui::Window::new("TreeGro").show(ctx, |ui| {
             if ui.button("Randomize").clicked() {
                 self.randomize();
@@ -48,10 +54,34 @@ impl App for World {
             ui.add(egui::Slider::new(&mut self.growth_rate, 0.0..=4.0));
 
             ui.add(egui::Slider::new(&mut self.time_delta, 0.1..=2.0));
+
+            ui.group(|ui| {
+                changed_size |= ui
+                    .add(
+                        egui::Slider::new(&mut self.pixels_size.0, 1..=WIDTH * 2)
+                            .text("Width")
+                            .logarithmic(true),
+                    )
+                    .changed();
+                changed_size |= ui
+                    .add(
+                        egui::Slider::new(&mut self.pixels_size.1, 1..=HEIGHT * 2)
+                            .text("Height")
+                            .logarithmic(true),
+                    )
+                    .changed();
+            });
         });
+
+        if changed_size {
+            pixels.resize_buffer(self.pixels_size.0, self.pixels_size.1);
+            self.randomize();
+            return;
+        }
 
         self.update_cells();
 
+        let frame = pixels.get_frame();
         for (cell, pixel) in self.cells.iter().zip(frame.chunks_exact_mut(4)) {
             let f = (cell.density * 256.0) as u8;
             pixel.copy_from_slice(&[f, f, f, 0xFF]);
